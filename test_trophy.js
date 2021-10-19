d0853e07bed7a6c696e45caf1204b389962ea885
Felixskz <72106708+Felixskz@users.noreply.github.com>
2021-10-19 18:42:41 +0100

    Update test_trophy.js

--- test_trophy.js
+++ test_trophy.js
@@ -1,7 +1,3 @@
-// Code created by @Felixskz Github
-// For use on a QnA page, e.g.: https://www.transum.org/software/SW/Starter_of_the_day/Students/Brackets.asp?Level=7
-// Press F12 and paste this code into the console. Then click on check answers
-// Author is not responsible for charges related to this code
 function checkAnswers() {
     $('#Trophybutton').show();
     $('#Trophybutton').val('trophy');
