f45dd7f912241f7c2959f2c90a068fb3d12a979a
Felixskz <72106708+Felixskz@users.noreply.github.com>
2022-09-16 15:15:47 +0100

    Update language_gym.js

--- language_gym.js
+++ language_gym.js
@@ -1,1 +1,7 @@
-document.getElementsByClassName("input-word apostrophe-answer").forEach(element => element.value = element.getAttribute("data-answer");)
+var questions = document.getElementsByClassName("input-word apostrophe-answer")
+for (var i = 0; i<questions.length; i++) {
+    var q = questions[i];
+    var a = q.getAttribute("data-answer");
+    q.value = a;
+    console.log(q)
+}
