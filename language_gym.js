5cf47113746cb3a80580cb8b61139af3632201e5
Felixskz <72106708+Felixskz@users.noreply.github.com>
2022-09-23 18:42:29 +0100

    Update language_gym.js

--- language_gym.js
+++ language_gym.js
@@ -6,3 +6,17 @@ for (var i = 0; i<questions.length; i++) {
     q.className = "input-word apostrophe-answer answered";
     console.log(q);
 }
+var questions = document.getElementsByClassName("question-answerwrap")
+for (var i = 0; i<questions.length; i++) {
+    for (var mySpan of questions[i].children) {
+        var a = mySpan.getAttribute("data-answer");
+        if (a == "correct") {
+            console.log("span:", mySpan, "a:", a);
+            GymGame.addCorrect();
+            mySpan.classList.add("answered");
+            mySpan.classList.add("answer-correct");
+    
+            questions[i].classList.add('answered');
+        }
+    }
+}
