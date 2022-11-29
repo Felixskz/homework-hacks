ff292eda5b8e71c3993dd7ef1b4fc829855db7b0
Felixskz <72106708+Felixskz@users.noreply.github.com>
2022-11-29 18:11:08 +0000

    Update verb_trainer.js

--- language gym/verb_trainer.js
+++ language gym/verb_trainer.js
@@ -1,14 +1,14 @@
-GymGame.hasTimer = false;
+GymGame.timerNextSecond = Number.MAX_SAFE_INTEGER; // pauses timer
 function delay(ms) {
     return new Promise(resolve => {
         setTimeout(() => {resolve('') }, ms);
     });
 }
 while (true) {
-    var question = document.getElementById("verb-trainer-inputanswer");
-    var answer = question.getAttribute("data-answer");
+    var question = document.getElementById("verb-trainer-inputanswer"); // textbox
+    var answer = question.getAttribute("data-answer"); // the answer
     question.value = answer;
     var nextButton = document.getElementById("check-answer");
-    nextButton.click();
+    nextButton.click(); // clicks the button to get to the next question
     await delay(200);
 }
