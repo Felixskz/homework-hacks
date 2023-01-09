0fdf90b2eb02499a558e22399c49dd77c0aecfbc
Felixskz <72106708+Felixskz@users.noreply.github.com>
2023-01-09 12:42:35 +0000

    Update match_up.js

--- language gym/match_up.js
+++ language gym/match_up.js
@@ -2,8 +2,8 @@ var leftHalf = document.getElementsByClassName("col-6 col-sm-6 list-left")[0]
 var rightHalf = document.getElementsByClassName("col-6 col-sm-6 list-right")[0]
 var colors = ['red', 'blue', 'green', 'yellow', 'pink', 'orange', 'white', 'purple', 'gray', 'cyan']
 
-left = leftHalf.children
-right = rightHalf.children
+var left = leftHalf.children
+var right = rightHalf.children
 
 var length = left.length
 
