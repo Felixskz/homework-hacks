c75ef2da891f4616b40e99ea83c99d43d979401b
Felixskz <72106708+Felixskz@users.noreply.github.com>
2023-01-09 10:28:49 +0000

    Create match_up.js

---
+++ language gym/match_up.js
@@ -0,0 +1,6 @@
+var leftHalf = document.getElementsByClassName("col-6 col-sm-6 list-left")[0]
+var rightHalf = document.getElementsByClassName("col-6 col-sm-6 list-right")[0]
+var colors = ['red', 'blue', 'green', 'yellow', 'pink', 'orange', 'white', 'purple', 'gray', 'cyan']
+colors = colors.slice(0, leftHalf.childCount
+
+//imcomplet
