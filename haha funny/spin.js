08ee1bd641f1a40d68465a0552a2af6914d26016
Felixskz <72106708+Felixskz@users.noreply.github.com>
2023-01-16 11:02:53 +0000

    Update and rename spin,js to spin.js

--- haha funny/spin.js
+++ haha funny/spin.js
@@ -1,11 +1,11 @@
-speed = 1
-element = document.getElementsByClassName(prompt("element?")[0]
+SPEED = 1
+element = document.getElementsByClassName(prompt("element class:"))[0]
 function delay(ms) {
     return new Promise(resolve => {
         setTimeout(() => {resolve('') }, ms);
     });
 }
 for (var i = 0; i < Number.MAX_VALUE; i++) {
-    element.style.setProperty("transform", "rotate("+i*speed+"deg)")
+    element.style.setProperty("transform", "rotate("+i*SPEED+"deg)")
     await delay(1)
 }
