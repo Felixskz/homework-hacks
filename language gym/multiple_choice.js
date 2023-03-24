dcdf61894a59028752905fd053092706037363af
Felixskz <72106708+Felixskz@users.noreply.github.com>
2023-03-24 10:06:16 +0000

    Update multiple_choice.js

--- language gym/multiple_choice.js
+++ language gym/multiple_choice.js
@@ -1,6 +1,6 @@
 function click(element) {
     element.click();
 }
-var answers = document.getElementsByClassName("word  answer")
+var answers = document.getElementsByClassName("word  answer") // sometimes the class name is not "word  answer" if it is instead for example "correct" just change it
 answers = Array.prototype.slice.call(answers)
 answers.forEach(click)
